ce3e356d79a0965a05a0c09e973aed6c5f9847e4
kx1t <15090643+kx1t@users.noreply.github.com>
2025-09-06 14:12:29 -0400

    Update noisecapt.sh

--- rootfs/usr/share/noisecapt/noisecapt.sh
+++ rootfs/usr/share/noisecapt/noisecapt.sh
@@ -210,9 +210,10 @@ while true; do
     DTIME="$(( ${PF_DELETEAFTER:-1} * 60 ))"
 	find "${OUTFILE%/*}" -name 'noisecapt-spectro-*.png' -mmin +"$DTIME" -delete
     find "${OUTFILE%/*}" -name 'noisecapt-recording-*.mp3' -mmin +"$DTIME" -delete
-	{ ls -1 "${OUTFILE%/*}" | gzip - > /tmp/outdir.gz && \
- 	  mv /tmp/outdir.gz "${OUTFILE}dir.gz" || \
+	{ ls -1 "${OUTFILE%/*}/*" | gzip - > /tmp/outdir.gz && \
+ 	  mv -f /tmp/outdir.gz "${OUTFILE}dir.gz" || \
 	  rm -f /tmp/outdir.gz
+      LOG "updated ${OUTFILE}dir.gz at $(date)"
      } &
 
     # clean up log file if necessary:
